0ab9a0bdffd22c2ca02f628d446eab0e6af35665
ignatenkomikhail <igna@inbox.ru>
2020-05-14 15:58:19 +0300

    Объединить два сортированных массива [#124482]

---
+++ test/ru/job4j/sort/MergeTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.sort;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore
+public class MergeTest {
+    @Test
+    public void whenBothEmpty() {
+        Merge algo = new Merge();
+        int[] expect = new int[0];
+        int[] result = algo.merge(
+                new int[0],
+                new int[0]
+        );
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenAscOrder() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2},
+                new int[] {3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenLeftLess() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 3, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2, 3},
+                new int[] {3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenLeftGreat() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4, 4};
+        int[] result = algo.merge(
+                new int[] {1, 2},
+                new int[] {3, 4, 4}
+        );
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenLeftEmpty() {
+        Merge algo = new Merge();
+        int[] expect = {1, 2, 3, 4};
+        int[] result = algo.merge(
+                new int[] {},
+                new int[] {1, 2, 3, 4}
+        );
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenLeftGreatFromNegative() {
+        Merge algo = new Merge();
+        int[] expect = {-4, -3, -2, 1, 3};
+        int[] result = algo.merge(
+                new int[] {-2, 1},
+                new int[] {-4, -3, 3}
+        );
+        assertThat(result, is(expect));
+    }
+}
