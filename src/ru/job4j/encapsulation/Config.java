77ec102e2ff42c68cbf8f21771aea802436e3323
ignatenkomikhail <igna@inbox.ru>
2020-05-18 19:13:41 +0300

    0. Что такое инкапсуляция [#284701]

--- src/ru/job4j/encapsulation/Config.java
+++ src/ru/job4j/encapsulation/Config.java
@@ -1,4 +1,4 @@
-package ru.job4j.inheritance;
+package ru.job4j.encapsulation;
 
 public class Config {
     private String name;
---
+++ src/ru/job4j/oop/Profession.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+
+public class Profession {
+
+    public String name;
+    public String surname;
+    public String birthday;
+    public String education;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+}
