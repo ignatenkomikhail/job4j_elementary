4be42326b45902d99e89f7976e383b8eab4ab837
ignatenkomikhail <igna@inbox.ru>
2020-05-18 06:39:40 +0300

    2. Перегрузка конструктора [#284687]

--- src/ru/job4j/oop/Predator.java
+++ src/ru/job4j/oop/Predator.java
@@ -2,6 +2,10 @@ package ru.job4j.oop;
 
 public class Predator extends Animal {
 
+    public Predator() {
+        System.out.println("Class Predator..");
+    }
+
     public Predator(String name) {
         super(name);
         System.out.println("Class Predator..");
--- src/ru/job4j/oop/Tiger.java
+++ src/ru/job4j/oop/Tiger.java
@@ -2,6 +2,10 @@ package ru.job4j.oop;
 
 public class Tiger extends Predator {
 
+    public Tiger() {
+        System.out.println("Class Tiger..");
+    }
+
     public Tiger(String name) {
         super(name);
         System.out.println("Class Tiger..");
