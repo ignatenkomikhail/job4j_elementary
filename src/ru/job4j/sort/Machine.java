738586180259c2fe15c1737ccf0883660f60f754
ignatenkomikhail <igna@inbox.ru>
2020-06-04 07:09:20 +0300

    1. Контрольные вопросы [791#284703]

---
+++ src/ru/job4j/sort/Machine.java
@@ -0,0 +1,23 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    public final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] result = new int[100];
+        int size = 0;
+        money -= price;
+        for (int coin : this.coins) {
+            while (money >= coin) {
+                money -= coin;
+                result[size++] = coin;
+            }
+            if (money == 0) {
+                break;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+}
