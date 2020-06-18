ad7dcac536313d6c9b3568919b9550f79c5fb6ec
ignatenkomikhail <igna@inbox.ru>
2020-06-18 18:54:16 +0300

    Проверить две строчки на идентичность [#273008]

--- src/ru/job4j/collection/FreezeStr.java
+++ src/ru/job4j/collection/FreezeStr.java
@@ -5,13 +5,23 @@ import java.util.*;
 public class FreezeStr {
     public static boolean eq(String left, String right) {
         boolean rslt = true;
-        List<String> list = new ArrayList<>(Arrays.asList(left.split("")));
+        /*List<String> list = new ArrayList<>(Arrays.asList(left.split("")));
         String[] array = right.split("");
         for (String s : array) {
             if (!list.remove(s)) {
                 rslt = false;
                 break;
             }
+        }*/
+        List<String> a = new ArrayList<>(Arrays.asList(left.split("")));
+        List<String> b = new ArrayList<>(Arrays.asList(right.split("")));
+        Iterator<String> it = b.iterator();
+        while (it.hasNext()) {
+            if (!a.remove(it.next())) {
+                rslt = false;
+                break;
+            }
+            it.remove();
         }
         return rslt;
     }
