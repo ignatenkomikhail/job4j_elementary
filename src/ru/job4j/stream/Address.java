8e87e5096fa48138dfd4d314070d7247f8f17022
ignatenkomikhail <igna@inbox.ru>
2020-06-22 20:48:45 +0300

    2. Список адресов [#284476]

--- src/ru/job4j/stream/Address.java
+++ src/ru/job4j/stream/Address.java
@@ -5,4 +5,27 @@ public class Address {
     private String street;
     private int home;
     private int apartment;
+
+    public Address(String city, String street, int home, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public int getApartment() {
+        return apartment;
+    }
 }
--- src/ru/job4j/stream/Profile.java
+++ src/ru/job4j/stream/Profile.java
@@ -6,9 +6,7 @@ import java.util.stream.Collectors;
 public class Profile {
     private Address address;
 
-    public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(s -> s.address)
-                .collect(Collectors.toList());
+    public Address getAddress() {
+        return address;
     }
 }
