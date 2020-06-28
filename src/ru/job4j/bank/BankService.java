d6bb69988db4a3c2acecf6486642084a9e07aefe
ignatenkomikhail <igna@inbox.ru>
2020-06-28 11:19:53 +0300

    3. Optional в банковских переводах [#284487]

--- src/ru/job4j/bank/BankService.java
+++ src/ru/job4j/bank/BankService.java
@@ -1,72 +1,68 @@
 package ru.job4j.bank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class BankService {
-    private Map<User, List<Account>> users = new HashMap<>();
+    private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
         this.users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
-        User user = this.findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = this.users.get(user);
+        Optional<User> user = this.findByPassport(passport);
+        if (user.isPresent()) {
+            List<Account> accounts = this.users.get(user.get());
             if (!accounts.contains(account)) {
                 accounts.add(account);
             }
         }
     }
 
-    public User findByPassport(String passport) {
-        /*User user = null;
+    public Optional<User> findByPassport(String passport) {
+        Optional<User> user = Optional.empty();
         for (User u : this.users.keySet()) {
             if (passport.equals(u.getPassport())) {
-                user = u;
+                user = Optional.of(u);
                 break;
             }
         }
-        return user;*/
-        return users.keySet().stream()
+        return user;
+        /*return users.keySet().stream()
                 .filter(e -> passport.equals(e.getPassport()))
                 .findFirst()
-                .orElse(null);
+                .orElse(null);*/
     }
 
-    public Account findByRequisite(String passport, String requisite) {
-        /*Account account = null;
-        User user = this.findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = this.users.get(user);
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<Account> account = Optional.empty();
+        Optional<User> user = this.findByPassport(passport);
+        if (user.isPresent()) {
+            List<Account> accounts = this.users.get(user.get());
             for (Account acc : accounts) {
                 if (requisite.equals(acc.getRequisite())) {
-                    account = acc;
+                    account = Optional.of(acc);
                     break;
                 }
             }
         }
-        return account;*/
-        User user = this.findByPassport(passport);
+        return account;
+        /*User user = this.findByPassport(passport);
         return (user == null) ? null
                 : this.users.get(user).stream()
                     .filter(e -> requisite.equals(e.getRequisite()))
                     .findFirst()
-                    .orElse(null);
+                    .orElse(null);*/
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account src = this.findByRequisite(srcPassport, srcRequisite);
-        Account dest = this.findByRequisite(destPassport, destRequisite);
-        if (src != null && dest != null && src.getBalance() >= amount) {
-            dest.setBalance(dest.getBalance() + amount);
-            src.setBalance(src.getBalance() - amount);
+        Optional<Account> src = this.findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> dest = this.findByRequisite(destPassport, destRequisite);
+        if (src.isPresent() && dest.isPresent() && src.get().getBalance() >= amount) {
+            dest.get().setBalance(dest.get().getBalance() + amount);
+            src.get().setBalance(src.get().getBalance() - amount);
             rsl = true;
         }
         return rsl;
